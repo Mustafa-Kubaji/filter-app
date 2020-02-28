6832119c6b2e6db1c8da3aeb86ef52567c38d063
ASUS <mustafa.kubaji.1@gmail.com>
2020-02-28 18:12:19 +0300

    third

--- app/src/main/java/com/example/imageprocessor/MainActivity.java
+++ app/src/main/java/com/example/imageprocessor/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView mainImage;
     // mustafaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
+	//muuuuuuuuuuuuuuuuuuuuathh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
